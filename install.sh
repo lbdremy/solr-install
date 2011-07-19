8ece1682c0cfbc9c554c475761c4418b420a5341
Remy Loubradou <remyloubradou@gmail.com>
2011-07-19 15:23:02 +0200

    Fix paths issues

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ cd /usr/local/
 JETTY_VERSION=7.4.2.v20110526
 sudo wget http://download.eclipse.org/jetty/$JETTY_VERSION/dist/jetty-distribution-$JETTY_VERSION.tar.gz
 sudo tar xfz jetty-distribution-$JETTY_VERSION.tar.gz
-cd jetty-distribution-$JETTY_VERSION
 sudo rm jetty-distribution-$JETTY_VERSION.tar.gz
 JETTY_HOME=/usr/local/jetty-distribution-$JETTY_VERSION
 
@@ -21,7 +20,7 @@ tar -xzf apache-solr-3.3.0.tgz
 rm apache-solr-3.3.0.tgz
 
 # Move Apache Solr Configuration File to Jetty directory
-sudo cp -R /tmp/apache-solr-3.3.0/example/solr/ $JETTY_HOME/jetty/
+sudo cp -R /tmp/apache-solr-3.3.0/example/solr/ $JETTY_HOME/
 
 # Copy Apache Solr Application (war file) to Jetty webapp directory
 sudo cp /tmp/apache-sorl-3.3.0/dist/apache-solr-3.3.0.war $JETTY_HOME/webapp/
