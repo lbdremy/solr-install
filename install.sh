ace7c813715e6ff28f2b5bb43f49cf7cbd75f6fa
Remy Loubradou <remyloubradou@gmail.com>
2011-07-19 13:46:26 +0200

    Clean-up

--- install.sh
+++ install.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 # Clone INSTALL JETTY AND SOLR
-git clone http://
-
-JAVA_HOME=/usr/bin/java
-JAVA_OPTIONS="-Dsolr.sorl?home=/usr/share/jetty/sorl/ $JAVA_OPTIONS"
-JETTY_HOME=/usr/share/jetty/solr/
-JETTY_USER=jetty
-JETTY_LOGS=/usr/share/jetty/logs/
-
-#Don't forget to edit /etc/default/jetty and change NO_START to be 0 (or comment it out)
+cd /tmp/
+git clone https://lbdremy@github.com/lbdremy/solr-install.git
 
 # Install dependencies
 sudo apt-get install openjdk-6-jre openjdk-6-jdk
@@ -18,11 +11,11 @@ sudo apt-get install openjdk-6-jre openjdk-6-jdk
 
 # Download and Install Jetty Server
 cd /usr/local/
-mkdir jetty
+sudo mkdir jetty
 cd jetty
 JETTY_VERSION=7.4.2.v20110526
-wget http://download.eclipse.org/jetty/$JETTY_VERSION/dist/jetty-distribution-$JETTY_VERSION.tar.gz
-tar xfz jetty-distribution-$JETTY_VERSION.tar.gz
+sudo wget http://download.eclipse.org/jetty/$JETTY_VERSION/dist/jetty-distribution-$JETTY_VERSION.tar.gz
+sudo tar xfz jetty-distribution-$JETTY_VERSION.tar.gz
 cd jetty-distribution-$JETTY_VERSION
 
 #Download and Install Apache Solr
@@ -36,6 +29,6 @@ cp -R /tmp/apache-solr-3.3.0/example/solr /usr/local/jetty/
 # Copy Apache Solr Application (war file) to Jetty webapp directory
 cp /tmp/apache-sorl-3.3.0/dist/apache-solr-3.3.0.war /usr/local/jetty/webapp/
 
-# Clone Solr context from git repository
-cd /usr/local/jetty/contexts/
+# Copy Solr context from git repository
+cp /tmp/solr-install/solr.xml /usr/local/jetty/contexts/
 
