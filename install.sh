e4ce7f06ea62c81ae1c9a277f140534bf936e2ea
Phillip Jackson <philwinkle@gmail.com>
2013-02-09 13:53:48 -0500

    Revert "Remove the -e option from bash to exit immediately"
    This reverts commit d0a0c61d88a2f59f2ac0768cf8a82c97e28e24fb.

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -e	 	
+# -e: Exit immediately if a command exits with a non-zero status.
 
 # Install dependencies depending of the distribution family
 echo "Download dependencies"
